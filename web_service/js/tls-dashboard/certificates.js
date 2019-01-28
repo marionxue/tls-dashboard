262f90352e62acab456562e562214aee1718d96d
marionxue <1451032707@qq.com>
2019-01-28 18:33:31 +0800

    update index.html

---
+++ web_service/js/tls-dashboard/certificates.js
@@ -0,0 +1,180 @@
+var run_date = 'Mon Jan 28 2019'; 
+var cert_info = {
+  "1": {
+    "server": {
+      "hostname": "www.xingshulin.com"
+    },
+    "subject": {
+      "common_name": "*.xingshulin.com",
+      "sans": "DNS:*.xingshulin.com, DNS:xingshulin.com"
+    },
+    "issuer": {
+      "org": "GoDaddy.com, Inc.",
+      "common_name": "Go Daddy Secure Certificate Authority - G2"
+    },
+    "info": {
+      "valid_from": "2017-04-06T05:02:00.000Z",
+      "valid_to": "2019-04-06T05:02:00.000Z",
+      "days_left": 68,
+      "sort_order": 68,
+      "background_class": "success"
+    }
+  },
+  "2": {
+    "server": {
+      "hostname": "www.xingshulinimg.com"
+    },
+    "subject": {
+      "common_name": "*.xingshulinimg.com",
+      "sans": "DNS:*.xingshulinimg.com, DNS:xingshulinimg.com"
+    },
+    "issuer": {
+      "org": "GoDaddy.com, Inc.",
+      "common_name": "Go Daddy Secure Certificate Authority - G2"
+    },
+    "info": {
+      "valid_from": "2018-08-25T12:44:09.000Z",
+      "valid_to": "2019-10-25T05:25:38.000Z",
+      "days_left": 270,
+      "sort_order": 270,
+      "background_class": "success"
+    }
+  },
+  "3": {
+    "server": {
+      "hostname": "www.baidu.com"
+    },
+    "subject": {
+      "org": "Beijing Baidu Netcom Science Technology Co., Ltd",
+      "common_name": "baidu.com",
+      "sans": "DNS:baidu.com, DNS:baifubao.com, DNS:www.baidu.cn, DNS:www.baidu.com.cn, DNS:mct.y.nuomi.com, DNS:baifae.com, DNS:apollo.auto, DNS:*.baidu.com, DNS:*.baifubao.com, DNS:*.baidustatic.com, DNS:*.bdstatic.com, DNS:*.bdimg.com, DNS:*.hao123.com, DNS:*.nuomi.com, DNS:*.chuanke.com, DNS:*.trustgo.com, DNS:*.bce.baidu.com, DNS:*.eyun.baidu.com, DNS:*.map.baidu.com, DNS:*.mbd.baidu.com, DNS:*.fanyi.baidu.com, DNS:*.baidubce.com, DNS:*.mipcdn.com, DNS:*.news.baidu.com, DNS:*.baidupcs.com, DNS:*.aipage.com, DNS:*.aipage.cn, DNS:*.bcehost.com, DNS:*.safe.baidu.com, DNS:*.im.baidu.com, DNS:*.ssl2.duapps.com, DNS:*.baifae.com, DNS:*.baiducontent.com, DNS:*.dlnel.com, DNS:*.dlnel.org, DNS:*.dueros.baidu.com, DNS:*.su.baidu.com, DNS:*.91.com, DNS:*.hao123.baidu.com, DNS:*.apollo.auto, DNS:*.xueshu.baidu.com, DNS:*.bj.baidubce.com, DNS:*.gz.baidubce.com, DNS:click.hm.baidu.com, DNS:log.hm.baidu.com, DNS:cm.pos.baidu.com, DNS:wn.pos.baidu.com, DNS:update.pan.baidu.com"
+    },
+    "issuer": {
+      "org": "GlobalSign nv-sa",
+      "common_name": "GlobalSign Organization Validation CA - SHA256 - G2"
+    },
+    "info": {
+      "valid_from": "2018-04-03T03:26:03.000Z",
+      "valid_to": "2019-05-26T05:31:02.000Z",
+      "days_left": 118,
+      "sort_order": 118,
+      "background_class": "success"
+    }
+  },
+  "4": {
+    "server": {
+      "hostname": "www.xingshulinih.com"
+    },
+    "subject": {
+      "common_name": "www.xingshulinih.com",
+      "sans": "DNS:www.xingshulinih.com, DNS:xingshulinih.com"
+    },
+    "issuer": {
+      "org": "GoDaddy.com, Inc.",
+      "common_name": "Go Daddy Secure Certificate Authority - G2"
+    },
+    "info": {
+      "valid_from": "2018-05-18T05:20:54.000Z",
+      "valid_to": "2019-05-18T05:20:54.000Z",
+      "days_left": 110,
+      "sort_order": 110,
+      "background_class": "success"
+    }
+  },
+  "5": {
+    "server": {
+      "hostname": "www.xsl.link"
+    },
+    "subject": {
+      "org": "Unknown",
+      "common_name": "The certificate has expired",
+      "sans": "Unknown"
+    },
+    "issuer": {
+      "org": "Unknown",
+      "common_name": "CERT_HAS_EXPIRED"
+    },
+    "info": {
+      "days_left": "0",
+      "sort_order": 0,
+      "background_class": "danger"
+    }
+  },
+  "6": {
+    "server": {
+      "hostname": "incomplete-chain.badssl.com"
+    },
+    "subject": {
+      "org": "Unknown",
+      "common_name": "The server provided a self-signed certificate or the provided certificate chain was incomplete",
+      "sans": "Unknown"
+    },
+    "issuer": {
+      "org": "Unknown",
+      "common_name": "UNABLE_TO_VERIFY_LEAF_SIGNATURE"
+    },
+    "info": {
+      "days_left": "--",
+      "sort_order": 100000,
+      "background_class": "error"
+    }
+  },
+  "7": {
+    "server": {
+      "hostname": "vsaq.dev"
+    },
+    "subject": {
+      "org": "Unknown",
+      "common_name": "An unspecified error occured",
+      "sans": "Unknown"
+    },
+    "issuer": {
+      "org": "Unknown",
+      "common_name": "ENOTFOUND"
+    },
+    "info": {
+      "days_left": "--",
+      "sort_order": 100000,
+      "background_class": "error"
+    }
+  },
+  "8": {
+    "server": {
+      "hostname": "example.dev",
+      "port": 8080
+    },
+    "subject": {
+      "org": "Unknown",
+      "common_name": "An unspecified error occured",
+      "sans": "Unknown"
+    },
+    "issuer": {
+      "org": "Unknown",
+      "common_name": "ENOTFOUND"
+    },
+    "info": {
+      "days_left": "--",
+      "sort_order": 100000,
+      "background_class": "error"
+    }
+  },
+  "9": {
+    "server": {
+      "hostname": "www.xingshulin.cn"
+    },
+    "subject": {
+      "org": "Unknown",
+      "common_name": "An unspecified error occured",
+      "sans": "Unknown"
+    },
+    "issuer": {
+      "org": "Unknown",
+      "common_name": "ETIMEDOUT"
+    },
+    "info": {
+      "days_left": "--",
+      "sort_order": 100000,
+      "background_class": "error"
+    }
+  }
+}
