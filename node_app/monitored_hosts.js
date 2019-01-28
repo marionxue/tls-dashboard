f7a57cd70fea85305ef2ba3196e7c896825de57e
marionxue <1451032707@qq.com>
2019-01-28 18:25:09 +0800

    update

---
+++ node_app/monitored_hosts.js
@@ -0,0 +1,31 @@
+var hosts = [
+  {
+    hostname: 'www.xingshulinimg.com'
+  },
+  {
+    hostname: 'www.xingshulin.com'
+  },
+  {
+    hostname: 'www.xingshulinih.com'
+  },
+  {
+    hostname: 'www.xsl.link'
+  },
+  {
+    hostname: 'www.xingshulin.cn'
+  },
+  {
+    hostname: 'www.baidu.com'
+  },
+  {
+    hostname: 'incomplete-chain.badssl.com'
+  },
+  {
+    hostname: 'example.dev',
+    port: 8080
+  },
+  {
+    hostname: 'vsaq.dev'
+  }
+];
+module.exports = hosts;
