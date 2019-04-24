923b3066d62da02d11ae3e2c552785a555580611
Marion <marion@MariondeMacBook-Pro.local>
2019-04-24 14:00:50 +0800

    update tls-dashboard

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #git add . && git commit -a -m "update tls-dashboard" && git push origin master
-docker build -t bluerdocker/tls-dashboard:v1 -f ./Dockerfile
+docker build -t bluerdocker/tls-dashboard:v1 -f ./Dockerfile .
