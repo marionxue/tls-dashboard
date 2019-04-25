12e088bcf7f4a862c5c556fdeec40fb60905ce2a
Your Name <you@example.com>
2019-04-25 18:26:19 +0800

    update

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-    cat /pod-data/tls-dashboard/monitered_hosts.js > /root/node_app/get_cert_info.js
+    cat /pod-data/tls-dashboard/monitered_hosts.js > /root/node_app/monitored_hosts.js
     /usr/local/bin/node /root/node_app/get_cert_info.js
     /bin/cp -R /root/web_service/* /pod-data/
     sleep 60
